03785281634f3f3d992c49c8296e05f7020b506d
mai-repo <ttmai@my.loyno.edu>
2025-01-21 15:40:23 -0500

    initialize Svelte app with basic structure and configuration

---
+++ src/.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<main>
+	<img src="" alt="Image">
+
+	<loginScreen>
+		<button type="button" id=btnLogin onclick=" "> Login </button>
+		<button type="button" id=btnSignUp onclick=""> Sign Up </button>
+		<button type="button" id=btnGuest onclick="">Login as a Guest</button>
+	</loginScreen>
+</main>
